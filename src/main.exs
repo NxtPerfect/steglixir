f39b2f78afbc3d9de46ebaf14405746ae495913f
NxtPerfect <alakaxan@protonmail.com>
2024-10-04 09:48:24 +0200

    Properly encrypt first three bits of message

--- src/main.exs
+++ src/main.exs
@@ -1,14 +1,14 @@
 import Bitwise
 
 defmodule Steglixir do
-  @spec encrypt(String, String) :: :ok
-  def encrypt(sourcePath, destinationPath)
+  @spec encrypt(String, String, String) :: :ok
+  def encrypt(sourcePath, destinationPath, message)
       when is_number(sourcePath) or is_nil(sourcePath) or is_number(destinationPath) or
-             is_nil(destinationPath),
+             is_nil(destinationPath) or is_number(message) or is_nil(message),
       do: "Path argument is empty or not a string"
 
-  @spec encrypt(String, String) :: :ok
-  def encrypt(sourcePath, destinationPath) do
+  @spec encrypt(String, String, String) :: :ok
+  def encrypt(sourcePath, destinationPath, message) when is_binary(message) do
     if not File.exists?(sourcePath) or File.dir?(sourcePath) do
       IO.puts("Source path doesn't exist or is a folder.")
       :error
@@ -26,13 +26,13 @@ defmodule Steglixir do
       :error
     end
 
-    [newRed, newGreen, newBlue] = divideFileToPixelsAndEncryptMessage(unencryptedFile, "Msg")
-    # green = green(& 1)
-    # blue = blue(& 1)
-    # testFile = <<red, green, blue>>
-    # IO.inspect([red, green, blue])
-    # IO.inspect(testFile)
+    [newRed, newGreen, newBlue, restOfFile] =
+      divideFileToPixelsAndEncryptMessage(unencryptedFile, "Msg")
+
     IO.inspect([newRed, newGreen, newBlue])
+    encryptedFile = [newRed, newGreen, newBlue, restOfFile]
+    File.write(destinationPath, encryptedFile)
+    encryptedFile
   end
 
   def decrypt(sourcePath, destinationPath) do
@@ -41,22 +41,21 @@ defmodule Steglixir do
     :ok
   end
 
-  @spec(divideFileToPixelsAndEncryptMessage(Binary, String) :: Binary, Binary, Binary)
+  @spec(divideFileToPixelsAndEncryptMessage(Binary, String) :: Binary, Binary, Binary, Binary)
   defp divideFileToPixelsAndEncryptMessage(unencryptedFile, message) do
-    <<rgb::binary-size(3), _restOfUnencryptedFile::binary>> = unencryptedFile
+    binaryMessage = :binary.first(message)
+    <<rgb::binary-size(3), restOfUnencryptedFile::binary>> = unencryptedFile
     <<red::binary-size(1), green::binary-size(1), blue::binary-size(1)>> = rgb
     IO.inspect([red, green, blue])
 
     # <<binaryMessage::utf8>> = message
 
-    # intMessageFirst = Enum.at(binaryMessage, 0)
-    <<firstChar::binary-size(1), _restOfIntMessageFirst::binary>> = message
-    # IO.inspect(<<intMessageFirst::binary>>)
+    [firstBit, secondBit, thirdBit | _restOfIntMessageFirst] = Integer.digits(binaryMessage, 2)
 
-    newRed = embedMessageIntoPixelColorChannel(firstChar, red)
-    newGreen = embedMessageIntoPixelColorChannel(firstChar, green)
-    newBlue = embedMessageIntoPixelColorChannel(firstChar, blue)
-    [newRed, newGreen, newBlue]
+    newRed = embedMessageIntoPixelColorChannel(firstBit, red)
+    newGreen = embedMessageIntoPixelColorChannel(secondBit, green)
+    newBlue = embedMessageIntoPixelColorChannel(thirdBit, blue)
+    [newRed, newGreen, newBlue, restOfUnencryptedFile]
   end
 
   @spec embedMessageIntoPixelColorChannel(Binary, Binary) :: Binary
@@ -68,7 +67,14 @@ defmodule Steglixir do
 
     <<colorChannelFirstSevenBits::size(7), colorChannelChangedLastBit::size(1)>>
   end
+
+  @spec readMessage(String) :: String
+  def readMessage(message) do
+    IO.puts(message)
+  end
 end
 
-Steglixir.encrypt("data/test.jpg", "data/encryptedTest.jpg")
+Steglixir.encrypt("data/test.jpg", "data/encryptedTest.jpg", "Hello, World!")
+Steglixir.readMessage("data/encryptedTest.jpg")
 Steglixir.decrypt("data/encryptedTest.jpg", "data/unencryptedTest.jpg")
+IO.puts("data/unencryptedTest.jpg" == "data/test.jpg")
